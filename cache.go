6cbe39f04d979a2de804dfc9867ec9b9b1fc8e91
snogo0 <99616238+snogo0@users.noreply.github.com>
2022-07-01 12:45:08 +0500

    Implement simple cache solution

--- cache.go
+++ cache.go
@@ -2,22 +2,48 @@ package cache
 
 import "time"
 
+type elem struct {
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	elems map[string]elem
+	delay time.Duration
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{elems: make(map[string]elem), delay: 55 * time.Millisecond}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	e, ok := c.elems[key]
+	if !ok {
+		return "", ok
+	}
+	return e.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.elems[key] = elem{value: value, deadline: time.Now()}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := make([]string, 0)
+	for key, _ := range c.elems {
+		keys = append(keys, key)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.elems[key] = elem{value: value, deadline: deadline}
+	go c.expire(key)
+}
+
+func (c *Cache) expire(key string) {
+	for c.elems[key].deadline.After(time.Now()) {
+		time.Sleep(c.delay)
+	}
+	delete(c.elems, key)
 }
